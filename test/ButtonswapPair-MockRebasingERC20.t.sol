418995f403e420d8e87859e5e5659f364749ae80
alphak3y <alphak3y@protonmail.com>
2024-08-30 14:26:34 -0600

    uncomment tests

--- test/ButtonswapPair-MockRebasingERC20.t.sol
+++ test/ButtonswapPair-MockRebasingERC20.t.sol
@@ -8,12 +8,12 @@ import {ICommonMockRebasingERC20} from
 
 // MockRebasingERC20 is known to suffer inaccuracies that break Pair math.
 // This is not considered blocking, so we disable the tests for now.
-//contract ButtonswapPairMockRebasingERC20Test is ButtonswapPairTest {
-//    function getRebasingTokenA() public override returns (ICommonMockRebasingERC20) {
-//        return ICommonMockRebasingERC20(address(new MockRebasingERC20("TokenA", "TKNA", 18)));
-//    }
-//
-//    function getRebasingTokenB() public override returns (ICommonMockRebasingERC20) {
-//        return ICommonMockRebasingERC20(address(new MockRebasingERC20("TokenB", "TKNB", 18)));
-//    }
-//}
+contract ButtonswapPairMockRebasingERC20Test is ButtonswapPairTest {
+   function getRebasingTokenA() public override returns (ICommonMockRebasingERC20) {
+       return ICommonMockRebasingERC20(address(new MockRebasingERC20("TokenA", "TKNA", 18)));
+   }
+
+   function getRebasingTokenB() public override returns (ICommonMockRebasingERC20) {
+       return ICommonMockRebasingERC20(address(new MockRebasingERC20("TokenB", "TKNB", 18)));
+   }
+}
